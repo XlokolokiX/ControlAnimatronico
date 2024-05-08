a702f1c067fab9f9072f4d4defaed6248ef31549
XlokolokiX <alfonsomouton@gmial.com>
2024-05-08 00:20:30 -0300

    Modificado para agregar el control de cada servo, parametrización general para cada servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,145 @@
+#include <SPI.h>
 #include <Arduino.h>
 #include <Ps3Controller.h>
-#include <ESP32Servo.h>
+#include <Adafruit_PWMServoDriver.h>
 #include <Adafruit_NeoPixel.h>
+
+/* La posición mínima y máxima del servo tiene que mapearse.
+ * Para los SG90 se tiene una frec de funcionamiento de 50Hz
+ * (T = 20ms) donde:
+ *  - DC = 1ms ----> 0°/-90° (todo a la izquierda)
+ *  - DC = 1.5ms --> 90°/0° (posición central) 
+ *  - DC = 2ms ----> 180°/90° (todo a la derecha)
+ *  
+ * El mapeo resulta:
+ * T = 20ms ----- 4095 cuentas
+ *  
+ * Pos_min = 1ms*4095/20ms   = 204 cuentas
+ * Pos_med = 1.5ms*4095/20ms = 307 cuentas
+ * Pos_max = 2ms*4095/20ms   = 409 cuentas
+ */
 
 //DEFINICIÓN DE PINES ------------------------------------------------------
 #define eye_LED_PIN 0
 #define torch_LED_PIN 4
-#define eye_LX_PIN 23
-#define eye_LY_PIN 22
-#define eye_RX_PIN 19
-#define eye_RY_PIN 18
-#define eye_LID1_PIN 25
-#define eye_LID2_PIN 26
-#define mouth_PIN 27
 //--------------------------------------------------------------------------
 
 //DEFINICIÓN DE CONFIGURACIONES --------------------------------------------
-#define LED_COUNT 8
-#define BRIGHTNESS 50
-#define MG90_MIN 500
-#define MG90_MAX 2400
-#define MG995_MIN 1000
-#define MG995_MAX 2000
+#define LED_COUNT 8       // N LED's en ambos ojos
+#define SERVOMIN  204     // ancho del pulso que da 0°/-90°
+#define SERVOMAX  409     // ancho del pulso que da 180°/90°
+#define SERVO_FREQ 50     // frecuencia de trabajo del servo
+#define SERVONUM   7      // cantidad de servos conectados al shield
+
+int servoPins[SERVONUM] = {0, 1};             
+// arreglo de servos conectados al shield 
+//0: up/down (EYE_LEFT)
+//l: left/right (EYE_LEFT)
+//2: up/down (EYE_RIGHT)
+//3: left/right (EYE_RIGHT)
+//4: UP_LID
+//5: DOWN_LID
+//6: MOUTH
+//--------------------------------------------------------------------------
+
+//MAPEO DE SERVOS ------(Grados)--------------------------------------------
+#define LX_MAX 90
+#define LX_MIN -90
+#define LY_MAX 90
+#define LY_MIN -90
+
+#define RX_MAX 90
+#define RX_MIN -90
+#define RY_MAX 90
+#define RY_MIN -90
+
+#define LIDUP_MAX 90
+#define LIDUP_MIN -90
+#define LIDOWN_MAX 90
+#define LIDOWN_MIN -90
+
+#define MOUTH_MAX 90
+#define MOUTH_MIN -90
 //--------------------------------------------------------------------------
 
 //VARIABLES GLOBALES -------------------------------------------------------
-
+//Luces
 Adafruit_NeoPixel eye_LED(LED_COUNT, eye_LED_PIN, NEO_GRB + NEO_KHZ800);
 bool turn_Eye_Led, turn_Flame_Led  = false;
 bool blue, red, green, pink = false;
 
-const char* address = "28:29:26:82:db:34";
+//Movimientos
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+int8_t analog_Data[1][2] = {{0,0}};
+int8_t mouth_Data = 0;
 
-int8_t analog_Data[2][2] = {{0,0},{0,0}};
+//BT
+const char* address = "28:29:26:82:db:34";          //ADDRESS DEL CONTROL PS3
 //--------------------------------------------------------------------------
 
 //ANIMACIONES --------------------------------------------------------------
 //--------------------------------------------------------------------------
 
+//FUNCIONES ----------------------------------------------------------------
+// Servo shield
+void setServo(uint8_t n_servo, long ang){
+  uint16_t duty = map(ang, -90, 90, SERVOMIN, SERVOMAX);
+  pwm.setPWM(n_servo, 0, duty);
+}
+
+void analogMapping(int8_t analogData, int8_t servo, int min_Angle, int max_Angle, int8_t death_Zone = 10, bool invert = false){
+  long angle;
+
+  if(analogData <= death_Zone && analogData >= -death_Zone){
+    angle = 0;
+  }else if(analogData >= -128 && analogData <= -death_Zone){
+    angle = map(analogData, -128, -death_Zone-1, min_Angle, -1);
+  }else if(analogData>=11 && analogData<=127){
+    angle = map(analogData, death_Zone+1, 127, 1, max_Angle);
+  }
+  if(invert){
+    angle = -angle;
+  }
+  setServo(servo, angle);
+  return;
+}
+
 void notify(){ //Eventos del control
   
-  //---------------- Analógicos ---------------
+  //---------------- Lógica Ojos ------------------------
   if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 2 ){
     analog_Data[0][0] = Ps3.data.analog.stick.lx;
     analog_Data[0][1] = Ps3.data.analog.stick.ly;
+    analogMapping(analog_Data[0][0], 0, LX_MIN, LX_MAX);
+    analogMapping(analog_Data[0][1], 1, LY_MIN, LY_MAX);
+    analogMapping(analog_Data[0][0], 2, RX_MIN, RX_MAX);
+    analogMapping(analog_Data[0][1], 3, RY_MIN, RY_MAX);
     Serial.println("L: " + (String)analog_Data[0][0] + " " +(String)analog_Data[0][1]);
   }
+  //---------------- Lógica pestañeo --------------------
 
-  if( abs(Ps3.event.analog_changed.stick.rx) + abs(Ps3.event.analog_changed.stick.ry) > 2 ){
-    analog_Data[1][0] = Ps3.data.analog.stick.rx;
-    analog_Data[1][1] = Ps3.data.analog.stick.ry;
-    Serial.println("R: " + (String)analog_Data[1][0] + " " +(String)analog_Data[1][1]);
+  //-----------------------------------------------------
+
+  //---------------- Lógica Boca ------------------------
+  if(Ps3.event.button_down.up == true){
+    while(Ps3.event.button_up.up == false){
+      if(mouth_Data >= -128 && mouth_Data <= 127){
+        delay(50);
+        mouth_Data += mouth_Data;
+        analogMapping(mouth_Data, 6, MOUTH_MIN, MOUTH_MAX);
+      }
+    }
   }
+  if(Ps3.event.button_down.down == true){
+    while(Ps3.event.button_up.down == false){
+      if(mouth_Data >= -128 && mouth_Data <= 127){
+        delay(50);
+        mouth_Data -= mouth_Data;
+        analogMapping(mouth_Data, 6, MOUTH_MIN, MOUTH_MAX);
+      }
+    }
+  }
+  //-----------------------------------------------------
 
   //---------------- Lógica de Luces ---------------------
   if(Ps3.event.button_down.r1 == 1){
@@ -118,6 +206,16 @@ void notify(){ //Eventos del control
       }
     }
   }
+  if(Ps3.event.button_down.r2 == 1){
+    turn_Flame_Led = !turn_Flame_Led;
+    Serial.println("Flame: " + (String)turn_Flame_Led);
+    if(turn_Flame_Led == true){
+      digitalWrite(torch_LED_PIN, 1);
+    }else{
+      digitalWrite(torch_LED_PIN, 0);
+    }
+  }
+  //-----------------------------------------------------
 }
 
 void connected(){
@@ -132,8 +230,9 @@ void disconnected(){
 }
 
 
+//--------------------------------------------------------------------------
 void setup() {
-  delay(200);
+  delay(1000);
   Serial.begin(115200);
   Serial.println(" ");
 
@@ -143,12 +242,22 @@ void setup() {
   Ps3.attachOnConnect(connected);
   Ps3.attachOnDisconnect(disconnected);
   
-  //Led
+  //Leds
   eye_LED.begin();
   eye_LED.setBrightness(255);
   eye_LED.show();
+  pinMode(torch_LED_PIN, OUTPUT);
 
+  //Servo shield
+  pwm.begin();
+  pwm.setPWMFreq(SERVO_FREQ);
+
+  for(int i = 0; i <= SERVONUM; i++){
+    setServo(servoPins[i], 0);
+  }
+  
   Serial.println("Ready");
+  delay(1000);
 }
 
 void loop() {
@@ -157,3 +266,4 @@ void loop() {
   }
   delay(100);
 }
+//--------------------------------------------------------------------------
